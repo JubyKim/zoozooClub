9653c314ea198d5e07f6502331c709edf29b4d67
JubyKim <70624771+JubyKim@users.noreply.github.com>
2025-10-18 14:38:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-cat > ~/teams-bot/app.py <<'PY'
+
 import os
 import logging
 from aiohttp import web
@@ -68,4 +68,4 @@ app.add_routes(routes)
 
 if __name__ == "__main__":
     web.run_app(app, host="0.0.0.0", port=PORT)
-PY
+
